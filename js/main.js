2816b801b639fcbfbb2c49eecc339c635e21bff7
RiinsSedows <112104511+RiinsSedows@users.noreply.github.com>
2022-11-14 03:58:31 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -61,3 +61,23 @@ function openTabs(evt, tabsName) {
   document.getElementById(tabsName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+var counterContainer = document.querySelector(".website-counter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
+resetButton.addEventListener("click", () => {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+  counterContainer.innerHTML = visitCount;
+});
