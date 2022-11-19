ad7d096bec3bc6d1e2870ef8a2d606e9b9009303
riinssedows <riinssedows@gmail.com>
2022-11-20 02:32:31 +0700

    addpreloader

--- js/main.js
+++ js/main.js
@@ -108,3 +108,12 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2500); // Change image every 2 seconds
 }
+
+$(document).ready(function() {
+ 
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 3000);
+ 
+});
