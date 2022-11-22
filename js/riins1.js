b60381dea6bcc91f84b643b3bcfcc31f8cce060f
riinssedows <riinssedows@gmail.com>
2022-11-22 19:28:55 +0700

    t

--- js/riins1.js
+++ js/riins1.js
@@ -71,39 +71,28 @@ function showSlides(n) {
 }
 
 
-$(document).on('click','.send', function(){
-  /* Inputan Formulir */
-  var input_name          = $("#name").val(),
-      input_email         = $("#email").val(),
-      input_phone         = $("#phone").val(),
-      input_product       = $("#product").val(),
-      input_description   = $("#description").val();
-
-  /* Pengaturan Whatsapp */
-  var walink      = 'https://web.whatsapp.com/send',
-      phone       = '62882006014023',
-      text        = 'Halo saya ingin memesan ',
-      text_yes    = 'Pesanan Anda berhasil terkirim.',
-      text_no     = 'Isilah formulir terlebih dahulu.';
-
-  /* Smartphone Support */
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-      var walink = 'whatsapp://send';
-  }
-
-  if(input_name != "" && input_phone != "" && input_product != ""){
-      /* Whatsapp URL */
-      var checkout_whatsapp = walink + '?phone=' + phone + '&text=' + text + '%0A%0A' +
-          '*Nama* : ' + input_name + '%0A' +
-          '*Alamat Email* : ' + input_email + '%0A' +
-          '*Nomor Kontak / Whatsapp* : ' + input_phone + '%0A' +
-          '*Produk* : ' + input_product + '%0A' +
-          '*Catatan* : ' + input_description + '%0A';
-
-      /* Whatsapp Window Open */
-      window.open(checkout_whatsapp,'_blank');
-      document.getElementById("text-info").innerHTML = '<div class="alert alert-success">'+text_yes+'</div>';
-  } else {
-      document.getElementById("text-info").innerHTML = '<div class="alert alert-danger">'+text_no+'</div>';
-  }
-});
+		$(document).on('click','.send', function(){
+			var input_name 			= $("#name").val(),
+			    input_email 		= $("#email").val(),
+			    input_phone 		= $("#phone").val(),
+			    input_product 		= $("#product").val(),
+			    input_description 	= $("#description").val();
+			var walink 		= 'https://web.whatsapp.com/send',
+			    phone 		= '62882006014023',
+			    text 		= 'Saya ingin membuat website : ',
+			    text_yes	= 'Pesanan Anda berhasil terkirim.',
+			    text_no 	= 'Isilah formulir terlebih dahulu.';
+			if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+				var walink = 'whatsapp://send';
+			}
+			if(input_name != "" && input_phone != "" && input_email != ""){
+				var checkout_whatsapp = walink + '?phone=' + phone + '&text=' + text + '%0A%0A' +
+				    '*Nama* : ' + input_name + '%0A' +
+				    '*Nama Toko* : ' + input_email + '%0A' +
+				    '*Link* : ' + input_description + '%0A';
+				window.open(checkout_whatsapp,'_blank');
+				document.getElementById("text-info").innerHTML = '<div class="alert alert-success">'+text_yes+'</div>';
+			} else {
+				document.getElementById("text-info").innerHTML = '<div class="alert alert-danger">'+text_no+'</div>';
+			}
+		});
