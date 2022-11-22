c67e71fa050243699ce8622f3087a9475d6cfda8
RiinsSedows <112104511+RiinsSedows@users.noreply.github.com>
2022-11-22 14:19:56 +0700

    update riins1.js

--- js/riins1.js
+++ js/riins1.js
@@ -76,8 +76,8 @@ var input_blanter = document.getElementById('wa_email');
 
 /* Whatsapp Settings */
 var walink = 'https://web.whatsapp.com/send',
-    phone = '6287825322716',
-    walink2 = 'Saya ingin bikin website ',
+    phone = '62882006014023',
+    walink2 = 'Saya mau bikin website',
     text_yes = 'Terkirim.',
     text_no = 'Isi semua Formulir lalu klik Send.';
 
@@ -92,14 +92,15 @@ if("" != input_blanter.value){
 var input_select1 = $("#wa_select :selected").text(),
     input_name1 = $("#wa_name").val(),
     input_email1 = $("#wa_email").val(),
+    input_number1 = $("#wa_number").val(),
     input_url1 = $("#wa_url").val(),
     input_textarea1 = $("#wa_textarea").val();
 
 /* Final Whatsapp URL */
 var blanter_whatsapp = walink + '?phone=' + phone + '&text=' + walink2 + '%0A%0A' +
     '*Nama* : ' + input_name1 + '%0A' +
-    '*Nama Toko* : ' + input_email1 + '%0A' +
-    '*Link Toko* : ' + input_url1 + '%0A' +
+    '*Nama toko* : ' + input_email1 + '%0A' +
+    '*Link request* : ' + input_url1 + '%0A' +
     '*Tambahan* : ' + input_textarea1 + '%0A';
 
 /* Whatsapp Window Open */
