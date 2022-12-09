c9ddc5ebf2bad11770691942efb76ef98a2aee56
riinssedows <riinssedows@gmail.com>
2022-12-09 23:21:28 +0700

    --

--- htdocs/komen/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('HOST','localhost');
-define('USER','riins');
-define('PASS','infinity6699');
-define('DB1', 'db_komen');
- 
-// Buat Koneksinya
-$db1 = new mysqli(HOST, USER, PASS, DB1);
-?>
